fa341b5e2a47646505e9833379d88d47e5570968
actuary <vernond@tcd.ie>
2022-08-13 16:34:35 +0100

    Added docker setup

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,7 +12,7 @@ SECRET_KEY = env(
     default="bFtcpkbhX2lYNOuLbYTpSnBjgOHM0S70OkurXN62bqbQieBs6iMvQahOLAbqayj4",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "django"]
 
 # CACHES
 # ------------------------------------------------------------------------------
