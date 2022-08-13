03c0f1f125ba434ca5b67a347b88b80660fccb63
actuary <vernond@tcd.ie>
2022-08-13 21:28:28 +0100

    changed url

--- config/settings/base.py
+++ config/settings/base.py
@@ -317,7 +317,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
-    "http://premeno-frontend.herokuapp.com"
+    "http://premeno-frontend.herokuapp.com",
     "https://premeno-frontend.herokuapp.com"
 ]
 # CORS_ORIGIN_ALLOW_ALL = True
